844fe1cf9a3be6ecd622a2f8dc3fe79386e02866
papa-channy <chan12cks@naver.com>
2025-06-06 19:21:27 +0900

    structure refactoring

--- 01_Web/exceptions/auth_exceptions.py
+++
@@ -1,6 +0,0 @@
-# 회원가입/로그인 관련 커스텀 예외 정의
-
-class DuplicateEmailError(Exception): pass
-class DuplicateUsernameError(Exception): pass
-class InvaluuidPasswordError(Exception): pass
-class UserNotFoundError(Exception): pass
